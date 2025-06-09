452f5a39b4d945bc1e88241e5a947d28b41e186d
Incerteza88 <info@incerteza.es>
2025-06-09 16:29:21 +0000

    Fin de proyecto

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-5">
+      Copyright 2025
+    </footer>
+  );
+};
+
+export default Footer;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,25 +3,43 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+//importo el Navbar
+import Navbar from "./Navbar";
+
+//importo el Jumbotron
+import Jumbotron from "./Jumbotron";
+
+//importo las cartas
+import Cards from "./Cards";
+
+//Importo footer
+import  Footer from "./Footer";
+
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-            
+			<Navbar />
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container-fluid my-5">
+
+				<div className="mb-5">
+					<Jumbotron />
+				</div>
+				<div className="row">
+					<Cards />
+					<Cards />
+					<Cards />
+					<Cards />
+				</div>
+			</div>
+
+			<Footer />
+
 		</div>
+
+
+
 	);
 };
 
